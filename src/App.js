8dc694d64e7e3b2c6ae9f7ce0201df8807af286a
Eric Rogers <Rogers.eric2012@gmail.com>
2021-09-21 14:02:15 -0500

    Added a hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 
 const imagePaths = {
@@ -6,25 +7,34 @@ const imagePaths = {
 }
 
 function clickMe(e) {
-  e.target.src === imagePaths.jenkins ? e.target.setAttribute("src", imagePaths.ducky) : e.target.setAttribute("src", imagePaths.jenkins);
+  const Results = e.target.src === imagePaths.jenkins ? e.target.setAttribute("src", imagePaths.ducky) : e.target.setAttribute("src", imagePaths.jenkins);
+  return Results
 }
 
-export const profile = (
-  <div>
-    <h1>I am Jenkins</h1>
-    <img
-      style={{maxWidth: '500px', maxHeight: '250px'}} 
-      src={imagePaths.jenkins}
-      alt="Jenkins The Elephant"
-      onClick={clickMe}
-    /><br />
-    <button onClick={clickMe}>Jenkins</button><br />
-    <article>
-      I like to sit<br />
-      Jenkins is my name<br />
-      Thanks a lot
-    </article>
-  </div>
-);
+function Profile() {
+  const [count, setCount] = useState(0);
 
-export default clickMe;
+  useEffect(() => {
+    document.title = `You clicked ${count} times.`
+  })
+
+  return (
+    <div>
+      <h1>I am Jenkins</h1>
+      <img
+        style={{maxWidth: '500px', maxHeight: '250px'}} 
+        src={imagePaths.jenkins}
+        alt="Jenkins The Elephant"
+        onClick={clickMe}
+      /><br />
+      <button onClick={clickMe}>Jenkins</button><br />
+      <article>
+        I like to sit<br />
+        Jenkins is my name<br />
+        Thanks a lot
+      </article>
+    </div>
+  );
+}
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
+import React from 'react';
 import ReactDOM from "react-dom";
 import "./index.css";
-import {profile} from "./App";
-
+import Profile from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(profile, rootElement);
-
-//ReactDOM.render(profile, document.getElementById("root"));
+ReactDOM.render(<Profile />, rootElement);
