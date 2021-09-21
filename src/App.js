ef9f4047ecc3b0f68648615670c7c5e5c2a58033
Eric Rogers <Rogers.eric2012@gmail.com>
2021-09-21 12:39:33 -0500

    Simplified the Jenkins site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const imagePaths = {
+  jenkins: "https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg",
+  ducky: "https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg"
 }
 
-export default App;
+function clickMe(e) {
+  e.target.src === imagePaths.jenkins ? e.target.setAttribute("src", imagePaths.ducky) : e.target.setAttribute("src", imagePaths.jenkins);
+}
+
+export const profile = (
+  <div>
+    <h1>I am Jenkins</h1>
+    <img
+      style={{maxWidth: '500px', maxHeight: '250px'}} 
+      src={imagePaths.jenkins}
+      alt="Jenkins The Elephant"
+      onClick={clickMe}
+    /><br />
+    <button onClick={clickMe}>Jenkins</button><br />
+    <article>
+      I like to sit<br />
+      Jenkins is my name<br />
+      Thanks a lot
+    </article>
+  </div>
+);
+
+export default clickMe;
