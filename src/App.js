7a5612ba4829662bb42b7167841b442f30cc85a5
Eric Rogers <Rogers.eric2012@gmail.com>
2021-09-21 14:38:47 -0500

    Corrected clickMe function to allow switching of image

--- src/App.js
+++ src/App.js
@@ -6,28 +6,30 @@ const imagePaths = {
   ducky: "https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg"
 }
 
-function clickMe(e) {
-  const Results = e.target.src === imagePaths.jenkins ? e.target.setAttribute("src", imagePaths.ducky) : e.target.setAttribute("src", imagePaths.jenkins);
-  return Results
-}
-
 function Profile() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     document.title = `You clicked ${count} times.`
-  })
+  },[count])
+
+  function clickMe() {
+    const target = document.getElementById('image')
+    target.src === imagePaths.jenkins ? target.setAttribute("src", imagePaths.ducky) : target.setAttribute("src", imagePaths.jenkins);
+    setCount(count + 1)
+  }
 
   return (
     <div>
       <h1>I am Jenkins</h1>
       <img
+        id='image'
         style={{maxWidth: '500px', maxHeight: '250px'}} 
         src={imagePaths.jenkins}
         alt="Jenkins The Elephant"
         onClick={clickMe}
       /><br />
-      <button onClick={clickMe}>Jenkins</button><br />
+      <button onClick={clickMe}>You clicked {count} times!</button><br />
       <article>
         I like to sit<br />
         Jenkins is my name<br />
